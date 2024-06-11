5c9aeda8d9e31b29ab685501b60833dcc76c051a
brlilie <151343648+brlilie@users.noreply.github.com>
2024-06-11 21:06:00 +0700

    Create konfirmasi pembayaran.cpp

---
+++ Memesan tiket bioskop /konfirmasi pembayaran.cpp
@@ -0,0 +1,13 @@
+//untuk membuat struk tiket pembayaran
+cout << "Tiket berhasil dipesan!" << endl;
+        cout<<"________________TIKET__________________"<<endl;
+        cout<<"\tFilm: "<<movie<<endl;
+        cout<<"\tHari: "<< day<<endl;
+        cout<<"\tJam: "<<time<<endl;
+        cout<<"\tKursi: "<<seat<<endl;
+        cout<<"\tJumlah tiket: "<<numTickets<<endl;
+        cout<<"\tKode Promo: "<<promoCode<<endl;
+        cout<<"\tTotal Harga: "<<totalPrice<<endl;
+        cout<<"\tPembayaran: "<<payment<<endl;
+        cout<<"\tKembalian: "<<change<<endl;
+        cout<<"______Terimakasih telah memesan!_______"<<endl;
